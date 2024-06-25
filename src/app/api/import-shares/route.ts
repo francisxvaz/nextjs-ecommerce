47abc4257ab54d69c9ec5982914a01e2afdcf11f
Francis Vaz <francisxvaz@gmail.com>
2024-06-26 08:35:58 +1000

    fix

--- src/app/api/import-shares/route.ts
+++ src/app/api/import-shares/route.ts
@@ -1,89 +1,55 @@
-import { NextApiResponse } from "next";
+import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from "@prisma/client";
+import data from "../.././../../data/latest-shares.json";
+
 const prisma = new PrismaClient();
-import data from '../.././../../data/latest-shares.json'
-import { NextRequest } from "next/server";
 
 type ProductType = {
-    name: string,
-    price: number,
-    percentageChange: number,
-    imageUrl: string,
-    description:string
+  name: string;
+  price: number;
+  percentageChange: number;
+  imageUrl: string;
+  description: string;
+};
+
+async function addProducts(product: ProductType) {
+  const { name, price, imageUrl, percentageChange, description } = product;
+  await prisma.product.create({
+    data: {
+      name,
+      price,
+      imageUrl,
+      percentageChange,
+      description,
+    },
+  });
 }
 
-export async function GET(req: NextRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest) {
   try {
-    data.map(async (share) => {
-        const name = share.name;
-        const price = Number(share.price) * 100;
-        const percentageChange = Number(share.percentageChange);
-        const description = share.name;
-        const imageUrl = `https://raw.githubusercontent.com/francisxvaz/nextjs-ecommerce/c1300ded38bb29139706bbb2c95d9d9d0b7b0471/public/logos/${name}.svg`;
+    await Promise.all(data.map(async (share) => {
+      const name = share.name;
+      const price = Number(share.price) * 100;
+      const percentageChange = Number(share.percentageChange);
+      const description = share.name;
+      const imageUrl = `https://raw.githubusercontent.com/francisxvaz/nextjs-ecommerce/c1300ded38bb29139706bbb2c95d9d9d0b7b0471/public/logos/${name}.svg`;
 
-        const product: ProductType = {
-            name,
-            price,
-            percentageChange,
-            description,
-            imageUrl
-        }
+      const product: ProductType = {
+        name,
+        price,
+        percentageChange,
+        description,
+        imageUrl,
+      };
 
-        await addProducts(product)
-        //updateProductPrice(name, price)
-      });
+      await addProducts(product);
+    }));
 
+    return NextResponse.json({ message: "Data updated successfully" }, { status: 200 });
   } catch (error) {
     console.error("Error updating data on Mongo:", error);
-    res.status(500).end("Internal Server Error");
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
-
-  async function addProducts(product: ProductType)
-  {
-    const {name, price, imageUrl, percentageChange, description} = product;
-    await prisma.product.create({
-        data: {
-            name,
-            price,
-            imageUrl,
-            percentageChange,
-            description
-        }
-    })
-    
-  }
-
-  async function updateProductPrice(name: string, newPrice: number) {
-    try {
-      // Find the product
-      const product = await prisma.product.findFirst({
-        where: {
-          name,
-        },
-      });
-
-      if (!product) {
-        throw new Error("Product not found");
-      }
-
-      product.price = newPrice;
-
-      const updatedProduct = await prisma.product.update({
-        where: {
-          id: product.id,
-        },
-        data: {
-          price: newPrice,
-        },
-      });
-
-      return updatedProduct;
-    } catch (error) {
-      console.error("Error updating product price:", error);
-      throw error;
-    } finally {
-      await prisma.$disconnect();
-    }
-  }
-
 }
+
+export const dynamic = 'force-dynamic';
